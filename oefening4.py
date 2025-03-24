a7f2a56f19a41be1a4e26220935edcef0855f398
maxroels <maxroels123@gmail.com>
2025-03-24 15:12:17 +0100

    oef 4 klaar

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,9 @@
-def countTargetPairs():
+def countTargetPairs(nums, target):
+    count = 0
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] < target:
+                count += 1
+    return count
+print(countTargetPairs([-1,1,2,3,1], 2))
